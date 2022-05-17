fc42f101c2c1d51e56ae5d8f02d17f4314710bdd
mac <gos360@163.com>
2022-05-17 13:35:32 +0800

    feat:login test

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,23 +1,15 @@
 import {FormEvent} from "react";
+import {login, register} from "../../auth/auth-provider";
 
 
-const apiUrl= process.env.REACT_APP_API_URL
 export const LoginScreen = () => {
-    const handleLogin =(params:{username:string,password:string})=>{
-        fetch(`${apiUrl}/login`,{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body:JSON.stringify(params)
-        },).then(res=>res.json())
-    }
+
     const handleSubmit = (event:FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         const username = (event.currentTarget.elements[0] as HTMLInputElement).value
         const password = (event.currentTarget.elements[1] as HTMLInputElement).value
         console.log(username, password)
-        handleLogin({username, password})
+        login({username, password})
     }
     return (
         <form onSubmit={handleSubmit}>
@@ -29,7 +21,7 @@ export const LoginScreen = () => {
                 <label htmlFor="password">password</label>
                 <input type={"password"} id="password"/>
             </div>
-            <button type="submit">Login</button>
+            <button type="submit">注册</button>
         </form>
     )
 }
---
+++ src/screens/search-panel.tsx
@@ -0,0 +1,39 @@
+import {useEffect, useState} from "react";
+
+export interface User {
+    id: string;
+    name: string;
+    title: string;
+    organization: string;
+    token: string;
+}
+
+interface SearchPanelProps {
+    users: User[];
+    param: {
+        name: string;
+        personId: string;
+    }
+    // 引用自身的定义
+    setParam: (param: SearchPanelProps['param']) => void;
+}
+
+export const SearchPanel = ({param, setParam, users}: SearchPanelProps) => {
+    return <form action="">
+        <div>
+
+            {/* 下面的写法等价于这个{setParam(Object.assign({}, param,{name:evt.target.name}))}*/}
+            <input type="text" value={param.name} onChange={(evt) => setParam({
+                ...param,
+                name: evt.target.value
+            })}/>
+            <select value={param.personId} onChange={(evt) => setParam({
+                ...param,
+                personId: evt.target.value
+            })}>
+                <option value="">负责人</option>
+                {users.map(v => <option key={v.id} value={v.id}>{v.name}</option>)}
+            </select>
+        </div>
+    </form>
+}
