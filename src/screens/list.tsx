86e256fa928d270370a5dcc6539df5d7f89726f6
mac <gos360@163.com>
2022-10-09 11:42:24 +0800

    feat:替换使用ant table组件

--- src/screens/list.tsx
+++ src/screens/list.tsx
@@ -1,4 +1,5 @@
 import {User} from "./search-panel";
+import {Table} from "antd";
 
 interface Project {
     id: string;
@@ -18,28 +19,38 @@ export const List = ({list, users}: ListProps) => {
     //     return users.find(user => item.personId === user.personId ? item.name : item.personId)
     // })
     // 下面的做法是直接输入显示了,这里的想法是想生成一个新的数组再到下面循环使用
-    return <table>
-        <thead>
-        <tr>
-            <th>名称</th>
-            <th>负责</th>
-        </tr>
-        </thead>
-        <tbody>
+    return <Table dataSource={list} columns={[
+        {title: '名称', dataIndex: 'name', key: 'name'},
         {
-            list.map(project => {
-                    return (
-                        <tr key={project.id}>
-                            <td>{project.name}</td>
-                            {/*这里需要好好琢磨一下,注意这里在map里面*/}
-                            {/*find 很可能会返回undefined,使用? 可以如果前面返回一个undefined.name则整个表达式都会返回undefined而不会报错
-                            也就是保证不出错可以配合||使用
-                            */}
-                            <td>{users.find(v => v.id === project.personId)?.name || '未知'}</td>
-                        </tr>)
-                }
-            )
-        }
-        </tbody>
-    </table>
+            title: '负责',
+            dataIndex: 'id',
+            key: 'id',
+            render: (_, project) => {
+                return <>{users.find(v => v.id === project.personId)?.name || '未知'}</>
+            }
+        }]}></Table>
+    // <table>
+    //     <thead>
+    //     <tr>
+    //         <th>名称</th>
+    //         <th>负责</th>
+    //     </tr>
+    //     </thead>
+    //     <tbody>
+    //     {
+    //         list.map(project => {
+    //                 return (
+    //                     <tr key={project.id}>
+    //                         <td>{project.name}</td>
+    //                         {/*这里需要好好琢磨一下,注意这里在map里面*/}
+    //                         {/*find 很可能会返回undefined,使用? 可以如果前面返回一个undefined.name则整个表达式都会返回undefined而不会报错
+    //                         也就是保证不出错可以配合||使用
+    //                         */}
+    //                         <td>{users.find(v => v.id === project.personId)?.name || '未知'}</td>
+    //                     </tr>)
+    //             }
+    //         )
+    //     }
+    //     </tbody>
+    // </table>
 }
